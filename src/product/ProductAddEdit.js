4168e4fd8d4dd2047ab607658252a67296acee44
xzacyy49 <407402590@gapp.fju.edu.tw>
2021-11-17 11:55:01 +0800

    柏妘衝鴨

--- src/product/ProductAddEdit.js
+++ src/product/ProductAddEdit.js
@@ -1,14 +1,13 @@
-import { doc, deleteDoc, addDoc, setDoc } from "firebase/firestore";
 import { useEffect } from 'react';
-import { collection } from '@firebase/firestore';
-import React, { useState } from 'react';
-
-
-
+import { useState } from 'react';
+import { getFirestore, collection, doc, addDoc, setDoc } from '@firebase/firestore';
+import { Dialog, DialogTitle, DialogContent, DialogActions } from "@mui/material";
+import { Button } from '@mui/material';
+import { TextField } from '@mui/material';
 
 export default function ProductAddEdit(props) {
-    const [product, setProduct] = useState({ desc: "", price: 0 })
-    const handleClickDesc = function (e) {
+    const [product, setProduct] = useState({ des: "", price: 0 })
+    const handleClickDes = function (e) {
         setProduct({ ...product, [e.target.name]: e.target.value })
     }
     useEffect(() => setProduct({ ...props.product }), [props.product]);
@@ -21,14 +20,14 @@ export default function ProductAddEdit(props) {
         try {
             if (action === "新增") {
                 const docRef = await addDoc(collection(db, "product"), {
-                    desc: product.desc,
+                    des: product.des,
                     price: parseInt(product.price)
                 });
                 console.log(docRef.id);
             }
             else {
                 await setDoc(doc(db, "product", product.id), {
-                    desc: product.desc,
+                    des: product.des,
                     price: parseInt(product.price)
                 });
             }
@@ -38,5 +37,23 @@ export default function ProductAddEdit(props) {
         }
         props.close();
     }
+    return (
+        <Dialog open={props.open}>
+            <DialogTitle>{action}產品</DialogTitle>
+            <DialogContent>
+                <TextField label="產品描述" name="des" variant="outlined" value={product.des} onChange={handleChange} />
+                <TextField label="產品價格" type="number" name="price" variant="outlined" value={product.price} onChange={handleChange} />
+            </DialogContent>
+            <DialogActions>
+                <Button variant="outlined" color="primary" onClick={update}>{action}</Button>
+                <Button variant="outlined" onClick={props.close}>取消</Button>
+            </DialogActions>
+        </Dialog>
+
+    );
 
 }
+
+
+
+
--- src/product/ProductList.js
+++ src/product/ProductList.js
@@ -8,42 +8,48 @@ import { initializeApp } from "firebase/app";
 import { config } from '../settings/firebaseConfig';
 import { collection } from '@firebase/firestore';
 import { useEffect } from 'react';
-import { getDocs,  doc, deleteDoc, updateDoc } from '@firebase/firestore';
+import { getDocs, doc, deleteDoc, } from '@firebase/firestore';
 import { CircularProgress } from '@mui/material';
-import { orderBy,getFirestore,query } from '@firebase/firestore';
-import ProductAdd from './ProductAdd';
-
-
-
+import { getFirestore, } from '@firebase/firestore';
+import { Fab } from '@mui/material';
+import ProductAddEdit from './ProductAddEdit';
 
 export default function ProductList() {
   const firebaseApp = initializeApp(config);
   const db = getFirestore();
   const [isLoading, setIsLoading] = useState(false);
-  const [open] = React.useState(false);
-  const deleted = useState(false);  
-  const [products, setProducts] = useState([
-    //   {desc:"iPad Light", price:20000},
-    //   {desc:"iPhone XR", price:30000},
-  ]);
+  const [open, setOpen] = React.useState(false);
+  const [deleted, setDeleted] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [currentProduct, setCurrentProduct] = useState(false);
+ 
 
   useEffect(() => {
     async function readData() {
       setIsLoading(true);
-      //const querySnapshot = await getDocs(collection(db, "product"));t
-      const querySnapshot = await getDocs(query(collection(db, "product"), orderBy("des")));
+      const querySnapshot = await getDocs(collection(db, "product"));
       const temp = [];
       querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
         temp.push({ id: doc.id, des: doc.data().des, price: doc.data().price });
+        console.log(doc.id);
+        console.log(doc.data().des);
       });
-      console.log(temp);
       setProducts([...temp]);
+      
       setIsLoading(false);
     }
     readData();
-  }, [db, open]);
+  }, [db, open, deleted]);
+
+  const addData = async function () {
+    setCurrentProduct({ des: "", price: 0 });
+    setOpen(true);
+  }
+  const editData = async function (index) {
+    console.log(index);
+    setCurrentProduct({...products[index]});
+    setOpen(true);
+  }
   const deleteData = async function (id) {
     try {
       setIsLoading(true);
@@ -57,38 +63,75 @@ export default function ProductList() {
     }
 
   }
+  const close = async function () {
+    setOpen(false);
+  }
+
   const ProductListComponent = function () {
     return (
       <List subheader="Product list" aria-label="product list">
         {products.map((product, index) =>
           <ListItem divider key={index}>
             <ListItemText primary={product.des} secondary={"NT$" + product.price}></ListItemText>
-            <IconButton edge="end" aria-label="create" onClick={() => deleteData(product.id)}>
-              <CreateIcon/>
+            <IconButton edge="end" aria-label="edit" onClick={() => editData(index)}>
+              <CreateIcon />
             </IconButton>
             <IconButton edge="end" aria-label="delete" onClick={() => deleteData(product.id)}>
               <DeleteIcon />
             </IconButton>
           </ListItem>)
-  }
+        }
       </List >
     )
+  }
+  return (
+    <Box sx={{
+      width: '100vw',
+      height: '100vh',
+      backgroundColor: 'background.paper',
+      color: 'black',
+      textAlign: 'left'
+    }}>
+      <AppMenu />
+      {!isLoading ?
+        <ProductListComponent />
+        :
+        <CircularProgress />
+      }
+      <Fab color="primary" aria-label="新增" onClick={addData}
+        sx={{
+          position: "fixed",
+          bottom: (theme) => theme.spacing(2),
+          right: (theme) => theme.spacing(8)
+        }}>
+        <CreateIcon />
+      </Fab>
+      <ProductAddEdit open={open} close={close} product={currentProduct} />
+    </Box>
+  );
 }
-return (
-  <Box sx={{
-    width: '100vw',
-    height: '100vh',
-    backgroundColor: 'background.paper',
-    color: 'black',
-    textAlign: 'left'
-  }}>
-    <AppMenu />
-    {!isLoading ?
-      <ProductListComponent />
-      :
-      <CircularProgress />
-    }
-    <ProductAdd />
-  </Box>
-);
-}
+/*
+<Box sx={{
+  width: '100vw',
+  height: '100vh',
+  backgroundColor: 'background.paper',
+  color: 'black',
+  textAlign: 'left'
+}}>
+  <AppMenu />
+  {!isLoading ?
+    <ProductListComponent />
+    :
+    <CircularProgress />
+  }
+  <Fab color="primary" aria-label="新增" onClick={addData}
+    sx={{
+      position: "fixed",
+      bottom: (theme) => theme.spacing(2),
+      right: (theme) => theme.spacing(8)
+    }}>
+    <AddIcon />
+  </Fab>
+  <ProductAddEdit open={open} close={close} product={currentProduct} />
+</Box>
+*/
