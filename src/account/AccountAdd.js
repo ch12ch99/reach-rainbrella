044ccbb8543f39c718242cfea8b4126d8b21d5b0
CHEN-YU-LAN <landy410436@gmail.com>
2021-11-24 11:31:35 +0800

    Account 還沒好

---
+++ src/account/AccountAdd.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { Dialog, DialogTitle, DialogContent, DialogActions } from "@mui/material";
+import { Button } from '@mui/material';
+import { TextField } from '@mui/material';
+import { getFirestore, collection, doc, addDoc, setDoc } from '@firebase/firestore';
+
+export default function AccountAddEdit(props) {
+    const [account, setAccount] = useState({ account_Status:"", machine_Id: 0 })
+    useEffect(() => setAccount({ ...props.account }), [props.account]);
+    const action = !props.account.id ? "新增" : "修改";
+    const handleChange = function (e) {
+        setAccount({ ...account, [e.target.name]: e.target.value })
+    }
+    const update = async function () {
+        const db = getFirestore();
+        try {
+            if (action === "新增") {
+                const docRef = await addDoc(collection(db, "account"), {
+                    account_Status: account.account_Status,
+                    machine_Id: parseInt(account.machine_Id)
+                });
+                console.log(docRef.id);
+            }
+            else {
+                await setDoc(doc(db, "account", account.id), {
+                    account_Status: account.account_Status,
+                    machine_Id: parseInt(account.machine_Id)
+                });
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+        props.close();
+    }
+    return (
+        <Dialog open={props.open}>
+            <DialogTitle>{action}帳戶</DialogTitle>
+            <DialogContent>
+                <TextField label="狀態" name="account_Status" variant="outlined" value={account.account_Status} onChange={handleChange} />
+                <TextField label="基台" type="number" name="machine_Id" variant="outlined" value={account.machine_Id} onChange={handleChange} />
+            </DialogContent>
+            <DialogActions>
+                <Button variant="outlined" color="primary" onClick={update}>{action}</Button>
+                <Button variant="outlined" color="secondary" onClick={props.close}>取消</Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
--- src/account/AccountList.js
+++ src/account/AccountList.js
@@ -1,19 +1,91 @@
-import React, { useState } from 'react';
-import { Box, List, ListItem, ListItemText } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Fab } from '@mui/material';
+import { CircularProgress } from '@mui/material';
+import { Box, List, ListItem, ListItemText, IconButton } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import CreateIcon from '@mui/icons-material/Create';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { initializeApp } from "firebase/app";
+import { doc, getDocs, deleteDoc } from '@firebase/firestore';
+import { getFirestore } from '@firebase/firestore';
+import { collection } from '@firebase/firestore';
+import { config } from '../settings/firebaseConfig';
 import AppMenu from '../ui/AppMenu';
+import AccountAddEdit from './AccountAddEdit';
 
 export default function AccountList() {
-    const [employees] = useState([
-        { id: "0", name: "Ben", department: "IT" },
-        { id: "1", name: "Rich", department: "Marketing" },
-        { id: "2", name: "Ruby", department: "Management" },
-        { id: "3", name: "Judy", department: "IT" },
-        { id: "4", name: "Rain", department: "IT" }
-    ]);
-    const [selectedIndex, setSelectedIndex] = useState(0);
-    const handleListItemClick = (index) => {
-        setSelectedIndex(index);
-    };
+    const firebaseApp = initializeApp(config);
+    const db = getFirestore();
+    const [open, setOpen] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [accounts, setAccounts] = useState([]); //useState是存firebase的資料 所以要用[]
+    const [currentAccount, setCurrentAccount] = useState(false);
+    const [deleted, setDeleted] = useState(false);
+
+    useEffect(() => {
+        async function readData() {
+          setIsLoading(true);
+          const querySnapshot = await getDocs(collection(db, "account")); // db後面是接table name 不是專案名稱
+          const temp = [];
+          querySnapshot.forEach((doc) => {
+            temp.push({id: doc.id,account_Id: doc.account_Id, account_Authority: doc.data().account_Authority, account_Id: doc.data().account_Id });
+            //自己設的_Id與系統給的id不同
+            console.log(doc.id);
+            console.log(doc.data().account_Authority);
+          });
+          setAccounts([...temp]);
+          setIsLoading(false);
+        }
+        readData();
+    }, [db, open, deleted]);
+    
+    const addData = async function () {
+        setCurrentAccount({ account_Authority:"", machine_Id: 0 });
+        setOpen(true);
+    }
+
+    const editData = async function (red) {
+      console.log(red);
+      setCurrentAccount({...accounts[red]});
+      setOpen(true);
+    }
+
+    const deleteData = async function (id) {
+        try {
+          setIsLoading(true);
+          await deleteDoc(doc(db, "account", id));
+          //console.log("deleted");
+          setDeleted(deleted + 1);
+          setIsLoading(false);
+        }
+        catch (error) {
+          console.log(error);
+        }
+      }
+
+    const close = async function () {
+        setOpen(false);
+      }
+
+
+      const AccountListComponent = function () {
+        return (
+            <List subheader="Account list" aria-label="account list">
+                {accounts.map((account, red) =>
+                  <ListItem divider key={red}>
+                    <ListItemText primary={"帳號名稱:" + account.account_Name} secondary={"E-mail:" + account.machine_Id + <br> + "E-mail:" + account.machine_Id + </br>}></ListItemText>
+
+                    <IconButton edge="end" aria-label="edit" onClick={() => editData(red)}>
+                      <CreateIcon />
+                    </IconButton>
+                    <IconButton edge="end" aria-label="delete" onClick={() => deleteData(account.id)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </ListItem>)
+                }
+              </List >
+        )
+    }
     return (
         <Box sx={{
             width: '100vw',
@@ -21,14 +93,22 @@ export default function AccountList() {
             backgroundColor: 'background.paper',
             color: 'black',
             textAlign: 'left'
-        }} >
-            <AppMenu/>
-            <List subheader="Account list" aria-label="Account list">
-                {employees.map((employee, index) =>
-                    <ListItem divider key={index} onClick={() => handleListItemClick(index)} selected={selectedIndex === index}>
-                        <ListItemText primary={employee.name} secondary={"@" + employee.department}></ListItemText>
-                    </ListItem>)}
-            </List>
-        </Box>
-    );
-}
+        }}>
+            <AppMenu />
+              {!isLoading ?
+                <AccountListComponent />
+                :
+                <CircularProgress />
+              }
+              <Fab color="primary" aria-label="新增" onClick={addData}
+                sx={{
+                  position: "fixed",
+                  bottom: (theme) => theme.spacing(2),
+                  right: (theme) => theme.spacing(8)
+                }}>
+                <AddIcon />
+              </Fab>
+              <AccountAddEdit open={open} close={close} account={currentAccount} />
+            </Box>
+          );
+        }
