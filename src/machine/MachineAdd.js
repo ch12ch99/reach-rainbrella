4169d99cc58278a8547a41eb75bc2b7bd2c1eecd
xzacyy49 <407402590@gapp.fju.edu.tw>
2021-11-24 10:23:30 +0800

    增加machine但讀不到資料

---
+++ src/machine/MachineAdd.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import { Dialog, DialogTitle, DialogContent, DialogActions } from "@mui/material";
+import { addDoc } from '@firebase/firestore';
+import { collection } from '@firebase/firestore';
+import { getFirestore } from "firebase/firestore";
+
+export default function MachineAdd(props) {
+    const db = getFirestore();
+    const addmachine = async function(){
+        try{
+            const docRef = await addDoc(collection(db,"machine"),{
+                machine_Address:parseInt(machine.machine_Address),
+                machine_Spaces:parseInt(machine.machine_Spaces)
+                //需要在哪個基台但我不會寫
+            });
+                console.log(docRef.id);
+                const temp = [];
+                docRef.forEach((doc) => {
+                    // doc.data() is never undefined for query doc snapshots
+                      console.log(doc.id, " => ", doc.data());
+                      temp.push({id: doc.id, machine_Id:doc.machine_Id, machine_Address:doc.data().machine_Address, machine_Spaces:doc.data().machine_Spaces});
+                    });
+            }catch(e) {
+                console.log(e);
+            }
+    }
+
+    const [machine, setMachine] = useState({ machineAddress:"", machine_Spaces: 0 })
+    const handleClick = function (e) {
+      setMachine({ ...machine, [e.target.name]: e.target.value })
+    }
+
+    const [open, setOpen] = useState(false);
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+    
+    return (
+        <div>
+            <Button variant="outlined" onClick={handleClickOpen}>新增</Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>機台</DialogTitle>
+                <DialogContent>
+                    機台位址:<input type="text" name="machine_Address" value={machine.machine_Address} onChange={handleClick} /><br />
+                    <br />
+                    機台空間:<input type="text" name="machine_Spaces" value={machine.machine_Spaces} onChange={handleClick} /><br />
+                </DialogContent>
+                <DialogActions>
+                    <button variant="outlined" onClick={addmachine}>新增</button>
+                    <button variant="outlined" onClick={handleClose}>關閉</button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
--- src/machine/MachineList.js
+++ src/machine/MachineList.js
@@ -1,19 +1,90 @@
-import React, { useState } from 'react';
-import { Box, List, ListItem, ListItemText } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Fab } from '@mui/material';
+import { CircularProgress } from '@mui/material';
+import { Box, List, ListItem, ListItemText, IconButton } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import CreateIcon from '@mui/icons-material/Create';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { initializeApp } from "firebase/app";
+import { doc, getDocs, deleteDoc } from '@firebase/firestore';
+import { getFirestore } from '@firebase/firestore';
+import { collection } from '@firebase/firestore';
+import { config } from '../settings/firebaseConfig';
 import AppMenu from '../ui/AppMenu';
+import MachineAddEdit from './MachineAddEdit';
 
 export default function MachineList() {
-    const [employees] = useState([
-        { id: "0", name: "Ben", department: "IT" },
-        { id: "1", name: "Rich", department: "Marketing" },
-        { id: "2", name: "Ruby", department: "Management" },
-        { id: "3", name: "Judy", department: "IT" },
-        { id: "4", name: "Rain", department: "IT" }
-    ]);
-    const [selectedIndex, setSelectedIndex] = useState(0);
-    const handleListItemClick = (index) => {
-        setSelectedIndex(index);
-    };
+    const firebaseApp = initializeApp(config);
+    const db = getFirestore();
+    const [open, setOpen] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    // const [umbrellas, setUmbrellas] = useState(false);
+    const [machines, setMachines] = useState([]); //useState是存firebase的資料 所以要用[]
+    const [currentMachine, setCurrentMachine] = useState(false);
+    const [deleted, setDeleted] = useState(false);
+    useEffect(() => {
+        async function readData() {
+          setIsLoading(true);
+          const querySnapshot = await getDocs(collection(db, "machine")); // db後面是接table name 不是專案名稱
+          const temp = [];
+          querySnapshot.forEach((doc) => {
+            temp.push({id: doc.id,machine_Id: doc.machine_Id, machine_Address: doc.data().machine_Address, machine_Spaces: doc.data().machine_Spaces });
+            //自己設的umbrella_Id與系統給的id不同
+            console.log(doc.id);
+            console.log(doc.data().machine_Address);
+            console.log(doc.data().machine_Spaces);
+          });
+          setMachines([...temp]);
+          setIsLoading(false);
+        }
+        readData();
+    }, [db, open, deleted]);
+
+    const addData = async function () {
+        setCurrentMachine({ machine_Address:"", machine_Spaces: 0 });
+        setOpen(true);
+    }
+
+    const editData = async function (duck) {
+      console.log(duck);
+      setCurrentMachine({...machines[duck]});
+      setOpen(true);
+    }
+
+    const deleteData = async function (id) {
+        try {
+          setIsLoading(true);
+          await deleteDoc(doc(db, "machine", id));
+          //console.log("deleted");
+          setDeleted(deleted + 1);
+          setIsLoading(false);
+        }
+        catch (error) {
+          console.log(error);
+        }
+      }
+
+    const close = async function () {
+        setOpen(false);
+      }
+
+    const MachineListComponent = function () {
+        return (
+            <List subheader="Machine list" aria-label="machine list">
+                {machines.map((machine, duck) =>
+                  <ListItem divider key={duck}>
+                    <ListItemText primary={"地址:" + machines.machine_Address} secondary={"空間:" + machines.machine_Spaces}></ListItemText>
+                    <IconButton edge="end" aria-label="edit" onClick={() => editData(duck)}>
+                      <CreateIcon />
+                    </IconButton>
+                    <IconButton edge="end" aria-label="delete" onClick={() => deleteData(machine.id)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </ListItem>)
+                }
+              </List >
+        )
+    }
     return (
         <Box sx={{
             width: '100vw',
@@ -21,14 +92,22 @@ export default function MachineList() {
             backgroundColor: 'background.paper',
             color: 'black',
             textAlign: 'left'
-        }} >
-            <AppMenu/>
-            <List subheader="Machine list" aria-label="Machine list">
-                {employees.map((employee, index) =>
-                    <ListItem divider key={index} onClick={() => handleListItemClick(index)} selected={selectedIndex === index}>
-                        <ListItemText primary={employee.name} secondary={"@" + employee.department}></ListItemText>
-                    </ListItem>)}
-            </List>
-        </Box>
-    );
-}
+        }}>
+            <AppMenu />
+              {!isLoading ?
+                <MachineListComponent />
+                :
+                <CircularProgress />
+              }
+              <Fab color="primary" aria-label="新增" onClick={addData}
+                sx={{
+                  position: "fixed",
+                  bottom: (theme) => theme.spacing(2),
+                  right: (theme) => theme.spacing(8)
+                }}>
+                <AddIcon />
+              </Fab>
+              <MachineAddEdit open={open} close={close} machine={currentMachine} />
+            </Box>
+          );
+        }
