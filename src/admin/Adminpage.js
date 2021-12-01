35603f8f3e81b53676a5e2dcdb52d7037ad14c3c
woody217088 <78297724+woody217088@users.noreply.github.com>
2021-12-01 11:59:29 +0800

    我回去會搞管理者頁面ㄉ 波給！！！

---
+++ src/admin/Adminpage.js
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from 'react';
+import { Fab } from '@mui/material';
+import { CircularProgress } from '@mui/material';
+import { Box, List, ListItem, ListItemText, IconButton } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import CreateIcon from '@mui/icons-material/Create';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { initializeApp } from "firebase/app";
+import { doc, getDocs, deleteDoc } from '@firebase/firestore';
+import { getFirestore } from '@firebase/firestore';
+import { collection } from '@firebase/firestore';
+import { config } from '../settings/firebaseConfig';
+import AppMenu from '../ui/AppMenu';
+import MachineAddEdit from './MachineAddEdit';
+
+export default function MachineList() {
+    const firebaseApp = initializeApp(config);
+    const db = getFirestore();
+    const [isLoading, setIsLoading] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [deleted, setDeleted] = useState(false);
+    const [machines, setMachines] = useState([]); //useState是存firebase的資料 所以要用[]
+    const [currentMachine, setCurrentMachine] = useState(false);
+
+    useEffect(() => {
+        async function readData() {
+          setIsLoading(true);
+          const querySnapshot = await getDocs(collection(db, "machine")); // db後面是接table name 不是專案名稱
+          const temp = [];
+          querySnapshot.forEach((doc) => {
+            temp.push({id: doc.id,machine_Id: doc.data().machine_Id, machine_Address: doc.data().machine_Address, machine_Spaces: doc.data().machine_Spaces });
+            //自己設的umbrella_Id與系統給的id不同
+            console.log(doc.id);
+            console.log(doc.data().machine_Address);
+            console.log(doc.data().machine_Spaces);
+          });
+          setMachines([...temp]);
+          setIsLoading(false);
+        }
+        readData();
+    }, [db, open, deleted]);
+
+    const addData = async function () {
+        setCurrentMachine({ machine_Address:"", machine_Spaces: 0 });
+        setOpen(true);
+    }
+
+    const editData = async function (duck) {
+      console.log(duck);
+      setCurrentMachine({...machines[duck]});
+      setOpen(true);
+    }
+
+    const deleteData = async function (id) {
+        try {
+          setIsLoading(true);
+          await deleteDoc(doc(db, "machine", id));
+          //console.log("deleted");
+          setDeleted(deleted + 1);
+          setIsLoading(false);
+        }
+        catch (error) {
+          console.log(error);
+        }
+      }
+    const close = async function () {
+        setOpen(false);
+      }
+
+    const MachineListComponent = function () {
+        return (
+            <List subheader="Machine list" aria-label="machine list">
+                {machines.map((machine, duck) =>
+                  <ListItem divider key={duck}>
+                    <ListItemText primary={"地址:" + machine.machine_Address} secondary={"空間:" + machine.machine_Spaces}></ListItemText>
+                    <IconButton edge="end" aria-label="edit" onClick={() => editData(duck)}>
+                      <CreateIcon />
+                    </IconButton>
+                    <IconButton edge="end" aria-label="delete" onClick={() => deleteData(machine.id)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </ListItem>)
+                }
+              </List >
+        )
+    }
+    return (
+        <Box sx={{
+          paddingLeft: '3vw',
+          width: '90vw',
+            height: '100vh',
+            backgroundColor: 'background.paper',
+            color: 'black',
+            textAlign: 'left'
+        }}>
+            
+              {!isLoading ?
+                <MachineListComponent />
+                :
+                <CircularProgress />
+              }
+              <Fab color="primary" aria-label="新增" onClick={addData}
+                sx={{
+                  position: "fixed",
+                  bottom: (theme) => theme.spacing(2),
+                  right: (theme) => theme.spacing(8)
+                }}>
+                <AddIcon />
+              </Fab>
+              <MachineAddEdit open={open} close={close} machine={currentMachine} />
+            </Box>
+          );
+        }
+        
---
+++ src/admin/MachineAddEdit.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { Dialog, DialogTitle, DialogContent, DialogActions } from "@mui/material";
+import { Button } from '@mui/material';
+import { TextField } from '@mui/material';
+import { getFirestore, collection, doc, addDoc, setDoc } from '@firebase/firestore';
+
+export default function MachineAddEdit(props) {
+    const [machine, setMachine] = useState({ machine_Address:"", machine_Spaces: 0 })
+    useEffect(() => setMachine({ ...props.machine }), [props.machine]);
+    const action = !props.machine.id ? "新增" : "修改";
+    const handleChange = function (e) {
+        setMachine({ ...machine, [e.target.name]: e.target.value })
+    }
+    const machineupdate = async function () {
+        const db = getFirestore();
+        try {
+            if (action === "新增") {
+                const docRef = await addDoc(collection(db, "machine"), {
+                    machine_Address: machine.machine_Address,
+                    machine_Spaces: parseInt(machine.machine_Spaces)
+                });
+                console.log(docRef.id);
+            }
+            else {
+                await setDoc(doc(db, "machine", machine.id), {
+                    machine_Address: machine.machine_Address,
+                    machine_Spaces: parseInt(machine.machine_Spaces)
+                });
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+        props.close();
+    }
+    return (
+        <Dialog open={props.open}>
+            <DialogTitle>{action}機台</DialogTitle>
+            <DialogContent>
+                <TextField label="機台位址" name="machine_Address" variant="outlined" value={machine.machine_Address} onChange={handleChange} />
+                <TextField label="機台空間" type="number" name="machine_Spaces" variant="outlined" value={machine.machine_Spaces} onChange={handleChange} />
+            </DialogContent>
+            <DialogActions>
+                <Button variant="outlined" color="primary" onClick={machineupdate}>{action}</Button>
+                <Button variant="outlined" color="secondary" onClick={props.close}>取消</Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
