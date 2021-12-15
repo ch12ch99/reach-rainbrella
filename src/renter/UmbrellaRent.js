fe36cca2039c5534a097b47d368cee44f3d72a0b
ch12ch99 <ch12ch99@gmail.com>
2021-12-15 08:04:04 +0800

    有stash等等修改

---
+++ src/renter/UmbrellaRent.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { Dialog, DialogTitle, DialogActions } from "@mui/material";
+import { Button } from '@mui/material';
+import { TextField } from '@mui/material';
+import { getFirestore, collection, doc, setDoc } from '@firebase/firestore';
+
+export default function UmbrellaRent(props) {
+    const [umbrella, setUmbrella] = useState({ machine_Id:"", umbrella_Status: 0 })
+    const [account, setAccount] = useState({ umbrella_Id: "" })
+    useEffect(() => setUmbrella({ ...props.umbrella }), [props.umbrella]);
+    const handleChange = function (e) {
+        setUmbrella({ ...umbrella, [e.target.name]: e.target.value })
+        setAccount({ ...account, [e.target.name]: e.target.value })
+    }
+    const umbrellaupdate = async function () {
+        const db = getFirestore();
+        try {
+            await setDoc(doc(db, "umbrella", umbrella.id), {
+                machine_Id: umbrella.machine_Id,
+                umbrella_Status: "租借中"
+            });
+            await setDoc(doc(db, "account", account.id), {
+                umbrella_Id: account.umbrella_Id,
+            });
+        }catch (e) {
+            console.log(e);
+        }
+        console.log("成功租借!");
+        props.close();
+    }
+    return (
+        <Dialog open={props.open}>
+            <DialogTitle>租借</DialogTitle>
+            <DialogActions>
+                <Button variant="outlined" color="primary" onClick={umbrellaupdate}></Button>
+                <Button variant="outlined" color="secondary" onClick={props.close}>取消</Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
--- src/renter/renter_MachineList.js
+++ src/renter/renter_MachineList.js
@@ -4,12 +4,13 @@ import { Button } from "@mui/material";
 import { initializeApp } from "firebase/app";
 import { doc, getDocs } from "@firebase/firestore";
 import { getFirestore } from "@firebase/firestore";
-import { collection } from "@firebase/firestore";
+import { collection, query, where } from "@firebase/firestore";
 import { config } from "../settings/firebaseConfig";
 import AppMenu from "../ui/AppMenu";
 import { AuthContext, STATUS } from "../account/AuthContext";
 import SignIn from "../account/SignIn";
 import SignUp from "../account/SignUp";
+<<<<<<< Updated upstream
  import "../account/SignIn.css";
  import { tableCellClasses } from '@mui/material/TableCell';
 import TableCell from "@mui/material/TableCell";
@@ -20,36 +21,54 @@ import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import { styled } from '@mui/material/styles';
 import TableContainer from "@mui/material/TableContainer";
+=======
+import UmbrellaRent from "./UmbrellaRent";
+import "../account/SignIn.css";
+>>>>>>> Stashed changes
 
 export default function MachineList() {
   const firebaseApp = initializeApp(config);
   const db = getFirestore();
   const [open, setOpen] = useState(false);
   const [machines, setMachines] = useState([]); //useState是存firebase的資料 所以要用[]
+  const [umbrellas, setUmbrellas] = useState([]);
   const [currentMachine, setCurrentMachine] = useState(false);
+  const [currentUmbrella, setCurrentUmbrella] = useState(false);
   const authContext = useContext(AuthContext);
 
   useEffect(() => {
     async function readData() {
       const querySnapshot = await getDocs(collection(db, "machine"));
+      const umbrellasameid = await getDocs(query(collection(db, "umbrella"), where('machine_Id', '==', 2)));
       const temp = [];
+      const temp2 = [];
       querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
-
         console.log(doc.id, " => ", doc.data());
         temp.push({
           id: doc.id,
           machine_Id: doc.data().machine_Id,
           machine_Address: doc.data().machine_Address,
-          machine_Spaces: doc.data().machine_Spaces,
+          machine_Spaces: doc.data().machine_Spaces
+        });
+      });
+      umbrellasameid.forEach((doc) =>{
+        console.log(doc.id, " => ", doc.data());
+        temp2.push({
+          id: doc.id,
+          umbrella_Id: doc.data().umbrella_Id,
+          umbrella_Status: doc.data().umbrella_Status
         });
       });
       console.log(temp);
-      setMachines([...temp]);
+      console.log(temp2);
+      setMachines([...temp]);      
+      setUmbrellas([...temp2]);
     }
     readData();
   }, [db]);
 
+<<<<<<< Updated upstream
   const editData = async function (duck) {
     console.log(duck);
     setCurrentMachine({ ...machines[duck] });
@@ -64,6 +83,17 @@ export default function MachineList() {
       fontSize: 30,
     },
   }));
+=======
+  const rent = async function (chicken) {
+    console.log(chicken);
+    setCurrentUmbrella({ ...umbrellas[chicken] });
+    setOpen(true);
+  };
+
+  const close = async function () {
+    setOpen(false);
+  };
+>>>>>>> Stashed changes
 
   const MachineListComponent = function () {
     return (
@@ -78,6 +108,7 @@ export default function MachineList() {
         </TableHead>
         <TableBody>
           {machines.map((machine, duck) => (
+<<<<<<< Updated upstream
             <TableRow
               key={machine.machine_Address}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -89,6 +120,19 @@ export default function MachineList() {
               <TableCell align="right">
             </TableCell>        
             </TableRow>
+=======
+            <ListItem divider key={duck}>
+                <ListItemText primary={"地址:" + machine.machine_Address} secondary={"空間:" + machine.machine_Spaces}
+                ></ListItemText>
+                {umbrellas.map((umbrella, chicken) => (
+                  <Button
+                  primary={umbrella.umbrella_Id}
+                  onClick={() => rent(chicken)}
+                  >
+                </Button>
+                ))}
+            </ListItem>
+>>>>>>> Stashed changes
           ))}
         </TableBody>
       </Table>
@@ -129,6 +173,7 @@ export default function MachineList() {
       ) : (
         <SignIn />
       )}
+      <UmbrellaRent open={open} close={close} umbrella={currentUmbrella} />
     </Box>
   );
 }
