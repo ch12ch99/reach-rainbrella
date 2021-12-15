6ec72ab33c05c6c1040c286deafd26ef3d190458
woody217088 <78297724+woody217088@users.noreply.github.com>
2021-12-15 10:21:31 +0800

    今天的東東 什麼手機也要能開ing

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import MachineList from "./machine/MachineList";
 import renter_MachineList from "./renter/renter_MachineList";
 import Main from "./ui/Main";
 import AppRouter from "./AppRouter";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const theme = createTheme({
   palette: {
@@ -45,6 +46,14 @@ ReactDOM.render(
 
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+
+// unregister() to register() below. Note this comes with some pitfalls.
+
+// Learn more about service workers: https://cra.link/PWA
+
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 
