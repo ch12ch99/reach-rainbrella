7261a708f5355f9e38baa25c1e37795cde941d0c
阿邱邱 <52485372+lol0920620797@users.noreply.github.com>
2022-01-12 11:36:08 +0800

    Update AppMenu.js

--- src/ui/AppMenu.js
+++ src/ui/AppMenu.js
@@ -6,9 +6,13 @@ import { AuthContext } from "../account/AuthContext";
 import Avatar from "@mui/material/Avatar";
 import { deepOrange } from "@mui/material/colors";
 import { LevelContext } from "../account/LevelContext";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
 import cat from "../cat.gif";
+import { getFirestore } from "@firebase/firestore";
+import { collection, query, where } from "@firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { doc, updateDoc, getDocs } from "@firebase/firestore";
 export default function AppMenu() {
   const authContext = useContext(AuthContext); //利用useContext hook取得AuthContext裡的值
   const levelContext = useContext(LevelContext);
@@ -16,31 +20,58 @@ export default function AppMenu() {
     typography: {
       htmlFontSize: 15,
       // Tell MUI what's the font-size on the html element is.
-    }
+    },
   });
   const darkTheme = createTheme({
     palette: {
       color: "blue",
       primary: {
-        main: '#7D7DFF',
+        main: "#7D7DFF",
       },
     },
   });
 
-  const meow = function () {
-    var random = Math.floor(Math.random() * 50);
-    console.log(random);
-    alert("喵~".repeat(random));
+  const meow = async function () {
+    const db = getFirestore();
+    const auth = getAuth();
+    const user = auth.currentUser;
+    const u_email = user.email;
+    console.log(u_email);
+    const accountconn = collection(db, "account");
+    const Result = query(accountconn, where("account_Email", "==", u_email));
+    const q1 = await getDocs(Result);
+    const put_umbrella = [];
+    q1.forEach((doc) => {
+      put_umbrella.push({
+        id: doc.id,
+        umbrella_Id: doc.data().umbrella_Id
+      });
+    });
+    const u_id = put_umbrella[0].id;
+    const umbrella_id = put_umbrella[0].umbrella_Id;
+    console.log(u_id,umbrella_id);
+    if (umbrella_id != 0) {
+      alert("back umbrella!");
+      const docRef = await updateDoc(doc(db, "account", u_id), {
+        umbrella_Id: "",
+      });
+    } else {
+      var random = Math.floor(Math.random() * 50);
+      console.log(random);
+      alert("喵~".repeat(random));
+    }
   };
 
   return (
     <ThemeProvider theme={darkTheme}>
-      <AppBar position="sticky" >
+      <AppBar position="sticky">
         <Toolbar>
           {levelContext.level === "administrator" ? (
             <Button component={Link} to="/product" color="inherit">
               <ThemeProvider theme={fonttheme}>
-                <Typography><strong>產品</strong></Typography>
+                <Typography>
+                  <strong>產品</strong>
+                </Typography>
               </ThemeProvider>
             </Button>
           ) : (
@@ -49,7 +80,9 @@ export default function AppMenu() {
           {levelContext.level === "administrator" ? (
             <Button component={Link} to="/account" color="inherit">
               <ThemeProvider theme={fonttheme}>
-                <Typography><strong>用戶</strong></Typography>
+                <Typography>
+                  <strong>用戶</strong>
+                </Typography>
               </ThemeProvider>
             </Button>
           ) : (
@@ -58,39 +91,51 @@ export default function AppMenu() {
           {levelContext.level === "administrator" ? (
             <Button component={Link} to="/umbrella" color="inherit">
               <ThemeProvider theme={fonttheme}>
-                <Typography><strong>雨傘</strong></Typography>
+                <Typography>
+                  <strong>雨傘</strong>
+                </Typography>
               </ThemeProvider>
             </Button>
           ) : (
             <Button component={Link} to="/umbrella" color="inherit">
               <ThemeProvider theme={fonttheme}>
-                <Typography><strong>雨傘</strong></Typography>
+                <Typography>
+                  <strong>雨傘</strong>
+                </Typography>
               </ThemeProvider>
             </Button>
           )}
           {levelContext.level === "administrator" ? (
             <Button component={Link} to="/machine" color="inherit">
               <ThemeProvider theme={fonttheme}>
-                <Typography><strong>機台</strong></Typography>
+                <Typography>
+                  <strong>機台</strong>
+                </Typography>
               </ThemeProvider>
             </Button>
           ) : (
             <Button component={Link} to="/machine" color="inherit">
               <ThemeProvider theme={fonttheme}>
-                <Typography><strong>機台</strong></Typography>
+                <Typography>
+                  <strong>機台</strong>
+                </Typography>
               </ThemeProvider>
             </Button>
           )}
           {levelContext.level === "administrator" ? (
             <Button component={Link} to="/renter" color="inherit">
               <ThemeProvider theme={fonttheme}>
-                <Typography><strong>租借雨傘</strong></Typography>
+                <Typography>
+                  <strong>租借雨傘</strong>
+                </Typography>
               </ThemeProvider>
             </Button>
           ) : (
             <Button component={Link} to="/renter" color="inherit">
               <ThemeProvider theme={fonttheme}>
-                <Typography><strong>租借雨傘</strong></Typography>
+                <Typography>
+                  <strong>租借雨傘</strong>
+                </Typography>
               </ThemeProvider>
             </Button>
           )}
@@ -102,7 +147,14 @@ export default function AppMenu() {
             ""
           )}
           {authContext.status === "signOut" ? (
-            <Avatar sx={{ bgcolor: deepOrange[50] }}><img src="https://anitar.dev/get/r" width="45" height="45" alt="Anitar" /></Avatar> //頭像Avatar 位置有點怪 帶修正
+            <Avatar sx={{ bgcolor: deepOrange[50] }}>
+              <img
+                src="https://anitar.dev/get/r"
+                width="45"
+                height="45"
+                alt="Anitar"
+              />
+            </Avatar> //頭像Avatar 位置有點怪 帶修正
           ) : (
             ""
           )}
@@ -111,7 +163,6 @@ export default function AppMenu() {
               <img src={cat} />
             </ThemeProvider>
           </Button>
-
         </Toolbar>
       </AppBar>
     </ThemeProvider>
